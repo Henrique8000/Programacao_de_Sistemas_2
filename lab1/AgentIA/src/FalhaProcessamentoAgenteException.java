a77b428ba70e8dff77bfbeac0395844b0eb14d29
Henrique <henriqueflavioguimaraes@gmail.com>
2026-02-24 21:01:40 -0300

    cloned prof solution for future refact

--- lab1/AgentIA/src/FalhaProcessamentoAgenteException.java
+++ lab1/AgentIA/src/FalhaProcessamentoAgenteException.java
@@ -1,19 +1,12 @@
-package model.exceptions;
-
 import java.time.LocalDateTime;
 
+// Base: Falha Geral
 public class FalhaProcessamentoAgenteException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-	
-	private final String timestamp;
-
+    private String timestamp;
     public FalhaProcessamentoAgenteException(String mensagem) {
         super(mensagem);
         this.timestamp = LocalDateTime.now().toString();
     }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
+    public String getTimestamp() { return timestamp; }
 }
+
