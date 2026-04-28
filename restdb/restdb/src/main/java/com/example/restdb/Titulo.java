ea0f5ce25611615cfac0ae1039910d0ac2b8396b
Henrique <henriqueflavioguimaraes@gmail.com>
2026-04-27 21:26:19 -0300

    feat: added titulo entity

--- restdb/restdb/src/main/java/com/example/restdb/Titulo.java
+++ restdb/restdb/src/main/java/com/example/restdb/Titulo.java
@@ -1,29 +1,30 @@
 package com.example.restdb;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@ToString 
 public class Titulo {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String nome;
 	private String esporte;
 	private Double premio;
 	
-	public Titulo(String nome, String esporte, Double premio) {
+	public Titulo() {
+	}
+	
+	public Titulo(long id, String nome, String esporte, Double premio) {
+		this.id = id;
 		this.nome = nome;
 		this.esporte = esporte;
 		this.premio = premio;
